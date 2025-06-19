0ff3a756d7b321320895b58d5a6a77ea3ebe2ec7
Phumeza_Sotshintshi <sotshintship@gmail.com>
2025-06-19 18:56:02 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const addButton = document.getElementById(`Add Task`);
+    const taskInput = taskInput.getElementById(`task-input`);
+    const taskList = taskList.getElementById(`task-list`);
+
+    function addTask(){
+        const taskText = taskInput.value.trim();
+        if (taskText===``){
+            alert(`Please enter a task`);
+            return;
+    
+        }
+        else{
+            const listItem = document.createElement(`li`);
+            listItem.textContent = taskText;
+   // Create remove button
+    const removeButton = document.createElement('button'); // Create "Remove" button
+    removeButton.textContent = 'Remove';
+    removeButton.className = 'remove-btn'; 
+
+    // Assign onclick event to remove the list item
+    removeButton.onclick = function () {
+      taskList.removeChild(li);
+
+        }
+    li.appendChild(removeButton);
+    taskList.appendChild(li);
+    taskInput.value = ``;
+
+
+        }
+
+    }
+
+})
